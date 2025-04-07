b50d267505ce2131105e50a867565116937c9a7a
rupankdas <128347589+rupankdas@users.noreply.github.com>
2025-04-07 09:04:16 +0800

    Update Program.cs

--- Week10/Program.cs
+++ Week10/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Hell0 Rupan");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
